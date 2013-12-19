4d28b4d7d23bb3206dbf7a11211f7a0146acbd23
scottmotte <scott@scottmotte.com>
2013-12-19 15:26:35 -0800

    Rename to smtpapi

--- test/main.js
+++ test/main.js
@@ -1,12 +1,12 @@
 var assert      = require('assert'),
     should      = require('should'),
-    xsmtpapi    = require('../lib/main');
+    smtpapi     = require('../lib/main');
 
 var result;
 
-describe('xsmtapi', function() {
+describe('smtapi', function() {
   before(function() {
-    result = xsmtpapi;
+    result = smtpapi;
   });
 
   it('version should be set', function() {
@@ -15,34 +15,34 @@ describe('xsmtapi', function() {
 
   describe('.Header', function() {
     it('has a toJsonString method', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.toJsonString().should.eql('{}');
     });
 
     it('addTo', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.addTo('addTo@mailinator.com');
       header.toJsonString().should.eql("{\"to\": [\"addTo@mailinator.com\"]}");
     });
 
     it('addSubVal', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.addSubVal('sub', 'val');
       header.toJsonString().should.eql("{\"sub\": {\"sub\":[\"val\"]}}");
     });
 
     it('setUniqueArgs', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.setUniqueArgs({set_unique_argument_key: 'set_unique_argument_value'});
       header.toJsonString().should.eql("{\"unique_args\": {\"set_unique_argument_key\":\"set_unique_argument_value\"}}");
     });
 
     it('addUniqueArgs', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.addUniqueArgs({add_unique_argument_key: 'add_unique_argument_value'});
       header.addUniqueArgs({add_unique_argument_key_2: 'add_unique_argument_value_2'});
@@ -50,14 +50,14 @@ describe('xsmtapi', function() {
     });
 
     it('setCategory', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.setCategory('setCategory');
       header.toJsonString().should.eql("{\"category\": [\"setCategory\"]}");
     });
 
     it('addCategory', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.addCategory('addCategory');
       header.addCategory('addCategory2');
@@ -65,14 +65,14 @@ describe('xsmtapi', function() {
     });
 
     it('setSection', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.setSection({'set_section_key': 'set_section_value'});
       header.toJsonString().should.eql("{\"section\": {\"set_section_key\":\"set_section_value\"}}");
     });
 
     it('addSection', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.addSection({'set_section_key': 'set_section_value'});
       header.addSection({'set_section_key_2': 'set_section_value_2'});
@@ -80,7 +80,7 @@ describe('xsmtapi', function() {
     });
 
     it('setFilterSetting', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       var filter = {
         'footer': {
@@ -96,7 +96,7 @@ describe('xsmtapi', function() {
     });
 
     it('addFilterSetting', function() {
-      var header = new xsmtpapi.Header();
+      var header = new smtpapi.Header();
 
       header.addFilterSetting('footer', 'text/html', '<strong>boo</strong>');
       header.toJsonString().should.eql("{\"filters\": {\"footer\":{\"settings\":{\"text/html\":\"<strong>boo</strong>\"}}}}");
