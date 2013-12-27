e261383163075e363282728b71128e49695f5151
scottmotte <scott@scottmotte.com>
2013-12-27 14:15:44 -0800

    Simplify and match up nearer the PHP library

--- test/main.js
+++ test/main.js
@@ -24,21 +24,21 @@ describe('smtapi', function() {
       var header = new smtpapi.Header();
 
       header.addTo('addTo@mailinator.com');
-      header.toJsonString().should.eql("{\"to\": [\"addTo@mailinator.com\"]}");
+      header.toJsonString().should.eql('{"to":["addTo@mailinator.com"]}');
     });
 
     it('addSubVal', function() {
       var header = new smtpapi.Header();
 
       header.addSubVal('sub', 'val');
-      header.toJsonString().should.eql("{\"sub\": {\"sub\":[\"val\"]}}");
+      header.toJsonString().should.eql('{"sub":{"sub":["val"]}}');
     });
 
     it('setUniqueArgs', function() {
       var header = new smtpapi.Header();
 
       header.setUniqueArgs({set_unique_argument_key: 'set_unique_argument_value'});
-      header.toJsonString().should.eql("{\"unique_args\": {\"set_unique_argument_key\":\"set_unique_argument_value\"}}");
+      header.toJsonString().should.eql("{\"unique_args\":{\"set_unique_argument_key\":\"set_unique_argument_value\"}}");
     });
 
     it('addUniqueArgs', function() {
@@ -46,14 +46,14 @@ describe('smtapi', function() {
 
       header.addUniqueArgs({add_unique_argument_key: 'add_unique_argument_value'});
       header.addUniqueArgs({add_unique_argument_key_2: 'add_unique_argument_value_2'});
-      header.toJsonString().should.eql("{\"unique_args\": {\"add_unique_argument_key\":\"add_unique_argument_value\",\"add_unique_argument_key_2\":\"add_unique_argument_value_2\"}}");
+      header.toJsonString().should.eql("{\"unique_args\":{\"add_unique_argument_key\":\"add_unique_argument_value\",\"add_unique_argument_key_2\":\"add_unique_argument_value_2\"}}");
     });
 
     it('setCategory', function() {
       var header = new smtpapi.Header();
 
       header.setCategory('setCategory');
-      header.toJsonString().should.eql("{\"category\": [\"setCategory\"]}");
+      header.toJsonString().should.eql("{\"category\":[\"setCategory\"]}");
     });
 
     it('addCategory', function() {
@@ -61,14 +61,14 @@ describe('smtapi', function() {
 
       header.addCategory('addCategory');
       header.addCategory('addCategory2');
-      header.toJsonString().should.eql("{\"category\": [\"addCategory\",\"addCategory2\"]}");
+      header.toJsonString().should.eql("{\"category\":[\"addCategory\",\"addCategory2\"]}");
     });
 
     it('setSection', function() {
       var header = new smtpapi.Header();
 
       header.setSection({'set_section_key': 'set_section_value'});
-      header.toJsonString().should.eql("{\"section\": {\"set_section_key\":\"set_section_value\"}}");
+      header.toJsonString().should.eql("{\"section\":{\"set_section_key\":\"set_section_value\"}}");
     });
 
     it('addSection', function() {
@@ -76,7 +76,7 @@ describe('smtapi', function() {
 
       header.addSection({'set_section_key': 'set_section_value'});
       header.addSection({'set_section_key_2': 'set_section_value_2'});
-      header.toJsonString().should.eql("{\"section\": {\"set_section_key\":\"set_section_value\",\"set_section_key_2\":\"set_section_value_2\"}}");
+      header.toJsonString().should.eql("{\"section\":{\"set_section_key\":\"set_section_value\",\"set_section_key_2\":\"set_section_value_2\"}}");
     });
 
     it('setFilterSetting', function() {
@@ -92,14 +92,14 @@ describe('smtapi', function() {
       }
 
       header.setFilterSetting(filter);
-      header.toJsonString().should.eql("{\"filters\": {\"footer\":{\"setting\":{\"enable\":1,\"text/plain\":\"You can haz footers!\"}}}}");
+      header.toJsonString().should.eql("{\"filters\":{\"footer\":{\"setting\":{\"enable\":1,\"text/plain\":\"You can haz footers!\"}}}}");
     });
 
     it('addFilterSetting', function() {
       var header = new smtpapi.Header();
 
       header.addFilterSetting('footer', 'text/html', '<strong>boo</strong>');
-      header.toJsonString().should.eql("{\"filters\": {\"footer\":{\"settings\":{\"text/html\":\"<strong>boo</strong>\"}}}}");
+      header.toJsonString().should.eql("{\"filters\":{\"footer\":{\"settings\":{\"text/html\":\"<strong>boo</strong>\"}}}}");
     });
 
   });
